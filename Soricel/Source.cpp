bcf7294a90d5e6ff7baefb01dc15b712d405ae39
Alessandra <vilcualessandra@gmail.com>
2025-03-02 14:19:06 +0200

    I cleaned the code

--- Soricel/Source.cpp
+++ Soricel/Source.cpp
@@ -3,114 +3,114 @@
 #include <fstream>
 #include <queue>
 
-typedef std::pair<int, int> pozitie;
+typedef std::pair<int, int> position;
 
-class Soarece
+class Mouse
 {
 private:
-	pozitie soarece, branza;
-	std::queue<pozitie> drum;
-	int** matrice;
-	int n, m;
+	position m_mouse, m_cheese;
+	std::queue<position> m_path;
+	int** m_matrix=nullptr;
+	int m_rows=0, m_column=0;
 public:
-	void citireMatrice()
+	void ReadMatrix()
 	{
 		std::ifstream f("date.txt");
-		f >> n >> m;
-		matrice = new int* [n];
-		for (int i = 0; i < n; i++)
+		f >> m_rows >> m_column;
+		m_matrix = new int* [m_rows];
+		for (int i = 0; i < m_rows; i++)
 		{
-			matrice[i] = new int[m];
-			for (int j = 0; j < m; j++)
-				f >> matrice[i][j];
+			m_matrix[i] = new int[m_column];
+			for (int j = 0; j < m_column; j++)
+				f >> m_matrix[i][j];
 		}
-		f >> soarece.first >> soarece.second;
-		f >> branza.first >> branza.second;
-		drum.push(soarece);
+		f >> m_mouse.first >> m_mouse.second;
+		f >> m_cheese.first >> m_cheese.second;
+		m_path.push(m_mouse);
 		f.close();
 	}
-	void dealocareMatrice()
+	void DeallocateMatrix()
 	{
-		for (int i = 0; i < n; i++)
-			delete[] matrice[i];
-		delete[] matrice;
+		for (int i = 0; i < m_rows; i++)
+			delete[] m_matrix[i];
+		delete[] m_matrix;
 	}
-	void drumul()
+	void Path()
 	{
-		matrice[soarece.first][soarece.second] = 0;
-		while (drum.front() != branza || drum.empty())
+		m_matrix[m_mouse.first][m_mouse.second] = 0;
+		while (m_path.front() != m_cheese || m_path.empty())
 		{
-			pozitie curenta = drum.front();
-			drum.pop();
-			if (curenta.second >= 1 && matrice[curenta.first][curenta.second - 1] == 0)
+			position current = m_path.front();
+			m_path.pop();
+			if (current.second >= 1 && m_matrix[current.first][current.second - 1] == 0)
 			{
-				matrice[curenta.first][curenta.second - 1] = matrice[curenta.first][curenta.second] + 1;
-				drum.push({ curenta.first,curenta.second - 1 });
+				m_matrix[current.first][current.second - 1] = m_matrix[current.first][current.second] + 1;
+				m_path.push({ current.first,current.second - 1 });
 			}
-			if (curenta.second < m - 1 && matrice[curenta.first][curenta.second + 1] == 0)
+			if (current.second < m_column - 1 && m_matrix[current.first][current.second + 1] == 0)
 			{
-				matrice[curenta.first][curenta.second + 1] = matrice[curenta.first][curenta.second] + 1;
-				drum.push({ curenta.first,curenta.second + 1 });
+				m_matrix[current.first][current.second + 1] = m_matrix[current.first][current.second] + 1;
+				m_path.push({ current.first,current.second + 1 });
 			}
-			if (curenta.first >= 1 && matrice[curenta.first - 1][curenta.second] == 0)
+			if (current.first >= 1 && m_matrix[current.first - 1][current.second] == 0)
 			{
-				matrice[curenta.first - 1][curenta.second] = matrice[curenta.first][curenta.second] + 1;
-				drum.push({ curenta.first - 1,curenta.second });
+				m_matrix[current.first - 1][current.second] = m_matrix[current.first][current.second] + 1;
+				m_path.push({ current.first - 1,current.second });
 			}
-			if (curenta.first < n - 1 && matrice[curenta.first + 1][curenta.second] == 0)
+			if (current.first < m_rows - 1 && m_matrix[current.first + 1][current.second] == 0)
 			{
-				matrice[curenta.first + 1][curenta.second] = matrice[curenta.first][curenta.second] + 1;
-				drum.push({ curenta.first + 1,curenta.second });
+				m_matrix[current.first + 1][current.second] = m_matrix[current.first][current.second] + 1;
+				m_path.push({ current.first + 1,current.second });
 			}
 		}
-		matrice[soarece.first][soarece.second] = 0;
-		if (drum.front() == branza)
+		m_matrix[m_mouse.first][m_mouse.second] = 0;
+		if (m_path.front() == m_cheese)
 		{
-			std::cout << "Drumul este\n";
-			int i = branza.first, j = branza.second;
+			std::cout << "The path is:\n";
+			int i = m_cheese.first, j = m_cheese.second;
 			std::cout << i << ' ' << j << '\n';
 			do
 			{
-				if (j >= 1 && matrice[i][j - 1] == (matrice[i][j] - 1))
+				if (j >= 1 && m_matrix[i][j - 1] == (m_matrix[i][j] - 1))
 				{
 					std::cout << i << ' ' << j - 1 << std::endl;
 					j--;
-					if (i == soarece.first && j == soarece.second)
+					if (i == m_mouse.first && j == m_mouse.second)
 						break;
 				}
-				if (j < m - 1 && matrice[i][j + 1] == (matrice[i][j] - 1))
+				if (j < m_column - 1 && m_matrix[i][j + 1] == (m_matrix[i][j] - 1))
 				{
 					std::cout << i << ' ' << j + 1 << std::endl;
 					j++;
-					if (i == soarece.first && j == soarece.second)
+					if (i == m_mouse.first && j == m_mouse.second)
 						break;
 				}
-				if (i >= 1 && matrice[i - 1][j] == (matrice[i][j] - 1))
+				if (i >= 1 && m_matrix[i - 1][j] == (m_matrix[i][j] - 1))
 				{
 					std::cout << i - 1 << ' ' << j << std::endl;
 					i--;
-					if (i == soarece.first && j == soarece.second)
+					if (i == m_mouse.first && j == m_mouse.second)
 						break;
 				}
-				if (i < n - 1 && matrice[i + 1][j] == (matrice[i][j] - 1))
+				if (i < m_rows - 1 && m_matrix[i + 1][j] == (m_matrix[i][j] - 1))
 				{
 					std::cout << i + 1 << ' ' << j << std::endl;
 					i++;
-					if (i == soarece.first && j == soarece.second)
+					if (i == m_mouse.first && j == m_mouse.second)
 						break;
 				}
-			} while (i != soarece.first || j != soarece.second);
+			} while (i != m_mouse.first || j != m_mouse.second);
 		}
 		else
-			std::cout << "Nu se poate ajunge la branza";
+			std::cout << "The cheese cannot be reached.";
 	}
 };
 
 int main()
 {
-	Soarece soarece;
-	soarece.citireMatrice();
-	soarece.drumul();
-	soarece.dealocareMatrice();
+	Mouse mouse;
+	mouse.ReadMatrix();
+	mouse.Path();
+	mouse.DeallocateMatrix();
 	return 0;
 }
